159aa8f2b9373a6b3b9adb0bb0a3ee0734c46c3c
erezka <soretatosha@gmail.com>
2019-07-02 21:43:34 +0400

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("my first one");
+        System.out.println("my first three");
 
         testClass human = new testClass("Володя",29);
 
