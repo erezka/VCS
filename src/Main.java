2867de8f31ee431665ffa1cc9b966233736719fc
erezka <soretatosha@gmail.com>
2019-07-02 21:30:32 +0400

    Test

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("my first one");
 
+        testClass human = new testClass("Володя",29);
+
+        System.out.println(human.getAge());
+
     }
+
 }
---
+++ src/testClass.java
@@ -0,0 +1,20 @@
+public class testClass {
+    public testClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public testClass() {
+    }
+
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
